b3ba0b0a72325e7260fa1752b5aecaa9629ab015
mustafa <mostafashahriar598@gmail.com>
2024-07-11 13:48:37 +0600

    bind a port try 2

--- app/server.go
+++ app/server.go
@@ -8,14 +8,20 @@ import (
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	defer l.Close()
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		defer conn.Close()
+		if err != nil {
+			fmt.Println("Failed to bind to port 4221")
+			os.Exit(1)
+		}
+		conn.Write([]byte("hello wolrd"))
 	}
+
 }
