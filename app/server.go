141b447fc3b55a2c0c43d1004128bb66db91dec8
mustafa <mostafashahriar598@gmail.com>
2024-07-11 18:57:06 +0600

    Return a file

--- app/server.go
+++ app/server.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
 
+var fileDir *string
+
 func main() {
+	fileDir = flag.String("directory", "/tmp/", "")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	defer l.Close()
 	if err != nil {
@@ -58,6 +62,25 @@ func handleConn(conn net.Conn) {
 		writeToConn(conn, "200 OK", header, userAgent)
 		return
 
+	}
+
+	if strings.HasPrefix(request.target, "/files/") {
+		path := strings.Split(request.target, "/")
+		pathParam := path[len(path)-1]
+
+		file, err := os.Open(*fileDir + "/" + pathParam)
+		if err != nil {
+			header := "Content-Type: application/octet-stream\r\n"
+			writeToConn(conn, "404 Not Found", header, "")
+			return
+		}
+		fileContent := make([]byte, 1024)
+		n, _ := file.Read(fileContent)
+		fileContent = fileContent[:n]
+
+		header := fmt.Sprintf("Content-Type: application/octet-stream\r\nContent-Length: %d\r\n", n)
+		writeToConn(conn, "200 OK", header, string(fileContent))
+		return
 	}
 
 	writeToConn(conn, "404 Not Found", "", "")
